9afedf51afc03742b41a22431c0347d507c91eb3
Thomas Leconte <thomas.leconte@agrial.com>
2024-11-01 15:40:57 +0100

    Facto: sqlite utils in dedicated package

---
+++ backend/commons/sqlite-base/index.ts
@@ -0,0 +1,9 @@
+import AbstractRepository from "./src/abstract-repository";
+import AbstractEntity from "./src/abstract-entity";
+import Database from './src/database';
+
+export {
+    Database,
+    AbstractEntity,
+    AbstractRepository
+}
